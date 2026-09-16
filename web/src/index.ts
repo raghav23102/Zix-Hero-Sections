62cdfef2b205779edab2d9b628716b975906dd16
agijaipur <androgamesinfotech@gmail.com>
2026-09-16 14:28:15 +0530

    Fix TypeScript compilation error with apiVersion

--- web/src/index.ts
+++ web/src/index.ts
@@ -39,7 +39,7 @@ const shopify = shopifyApp({
     apiSecretKey: process.env["SHOPIFY_API_SECRET"] ?? "",
     scopes: process.env.SCOPES ? process.env.SCOPES.split(",") : ["write_themes", "read_themes"],
     hostName: SHOPIFY_APP_URL.replace(/https?:\/\//, "").replace(/\/$/, ""),
-    apiVersion: LATEST_API_VERSION,
+    apiVersion: LATEST_API_VERSION as any,
     isEmbeddedApp: true,
   },
   useOnlineTokens: false,
